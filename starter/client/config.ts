128e6f21d92c2c95eb0960f6e4f7a4f2b82e1209
lucene-nguyen <manhcuongmvc@gmail.com>
2024-11-28 17:54:16 +0700

    add config file

---
+++ starter/client/config.ts
@@ -0,0 +1,8 @@
+const apiId = "4gxv9d3bda"
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: "dev-7brd6quydpm30rlu.us.auth0.com",
+  clientId: "RLtFh4P57SqEPLLSYEbS5vGhYu6dyCte",
+  callbackUrl: "http://localhost:3000"
+}
